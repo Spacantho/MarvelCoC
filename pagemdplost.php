03288b26803275505cbad04a22f2c00a289b214c
erwantaupin <taupin.erwan@laposte.net>
2022-03-30 15:49:31 +0200

    front-conex

---
+++ pagemdplost.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <title>Connexion</title>
+</head>
+<body>
+    <div class="box-body">
+        <div class="mask-body">
+            <div class="bandeau"></div>
+                <div class="box-titre-image">
+                    <div class="image-titre"></div>
+                </div>
+                <div class="box-body-input">
+                    <div class="box-imput">
+                        <div class="titre-page-info"><h2>PASSWORD PERDU</h2></div>
+                        <div class="message-php"><p></p></div>
+                            <form action="" method="post">
+                                <div class="imputi">
+                                    <label for="Pseudo"><p class="texte-pseudo">Pseudo:</p></label>
+                                    <input type="text" name="Pseudo" id="Pseudo" required><br>
+                                    <label for="mail"><p class="texte-pseudo">mail:</p></label>
+                                    <input type="mail" name="mail" id="mail" required>
+                                </div>
+                                <div class="validation">
+                                    <input class="btn-valide" type="submit" value="Valider">
+                                </div>
+                            </form>
+                        <div class="redirection">
+                            <div class="nouveau"><p>vous êtes nouveau?</p><a href="pageinscription.php"> inscrivez-vous ici</a></div>
+                        </div>
+                    </div>
+                </div>
+            <div class="bandeau1">
+                <div class="copy"></div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+
