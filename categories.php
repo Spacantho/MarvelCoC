cf50bad09757ccc508241fe3d9d1d6326baed50b
Eldryr62570 <jordan.moulin62570@gmail.com>
2022-03-31 08:05:11 +0200

    categorie

---
+++ categories.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/categories.css">
+    <title>Catégories</title>
+</head>
+<body>
+    <div class="fakenav"></div>
+    <section class="categorieContainer">
+        <div class="categories">
+            <div class="categorie">
+                <div class="titleCat">Catégorie 1</div>
+            </div>
+            <div class="categorie">
+                
+            </div>
+            <div class="categorie">
+                
+            </div>
+            <div class="categorie">
+                
+            </div>
+            <div class="categorie">
+                
+            </div>
+            <div class="categorie">
+                
+            </div>
+            <div class="categorie">
+                
+            </div>
+            
+            
+        </div>
+    </section>
+</body>
+</html>
