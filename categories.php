10835fa981dc7c47aa8bda49e6261882356ec4dd
Eldryr62570 <jordan.moulin62570@gmail.com>
2022-03-31 10:19:48 +0200

    Cases catégories + images

--- categories.php
+++ categories.php
@@ -11,8 +11,12 @@
     <div class="fakenav"></div>
     <section class="categorieContainer">
         <div class="categories">
-            <div class="categorie">
-                <div class="titleCat">Catégorie 1</div>
+            <div class="categorie" style="background-image: url('assets/img/placeholder.jpg');background-size: cover;">
+                <div class="filterBlack">
+                    <div class="titleCat">Catégorie 1</div>
+                    <div class="textCat"> <div> 0 vidéos</div>   </div>
+                </div>
+
             </div>
             <div class="categorie">
                 
