5839075871eacce3ef4e9f91b8849db4d861a0af
erwantaupin <taupin.erwan@laposte.net>
2022-04-04 14:49:34 +0200

    footer et font

--- assets/include/footer.php
+++ assets/include/footer.php
@@ -1,25 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>body-only</title>
-</head>
-<body>
     <footer>
         <div class="body-footer">
             <div class="box-footer">
-                <div class="copy">&copy; <p>Copyrights by Mr</p><a href="">Moulin,</a><p> </p><a href="">Genonceau,</a><p> </p><a href="">Tourolle</a><p> et </p><a href="">Taupin</a></div>
+                <div class="copy">&copy; <p class='textefooter'>Copyrights by Mr </p><a class='textefooter' href=""> Moulin, </a><p> </p><a class='textefooter' href="">Genonceau, </a><p> </p><a class='textefooter' href="">Tourolle </a><p> et </p><a class='textefooter' href="https://erwan.simplon-charleville.fr/mon-portfolio/mon-folio.php?envoi=1">Taupin </a></div>
                 <div class="link">
-                    <ul>
-                        <li><a href=""><img class="logo-reseaux" src="https://fakeimg.pl/250x100/"></a></li>
-                        <li><a href=""><img class="logo-reseaux" src="https://fakeimg.pl/250x100/"></a></li>
-                        <li><a href=""><img class="logo-reseaux" src="https://fakeimg.pl/250x100/"></a></li>
-                    </ul>
+                    <a href=""><img class="logo-reseaux" src="https://fakeimg.pl/250x100/"></a>
+                    <a href=""><img class="logo-reseaux" src="https://fakeimg.pl/250x100/"></a>
+                    <a href=""><img class="logo-reseaux" src="https://fakeimg.pl/250x100/"></a>
                 </div>
             </div>
         </div>
     </footer>
-</body>
-</html>
