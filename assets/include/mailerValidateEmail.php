4d706025c2a6236d6b436390e894ee489ce12829
Spacantho <anthony.tourolle08@gmail.com>
2022-04-04 15:19:08 +0200

    mailer2

--- assets/include/mailerValidateEmail.php
+++ assets/include/mailerValidateEmail.php
@@ -7,19 +7,26 @@
     $subject = "Validation de compte";
 
     $message = "
-    Bonjour $name,
-    Merci d’avoir rejoint MarvelCoC.
-    
-    Nous aimerions vous confirmer que votre compte a été créé avec succès. Pour accéder à MarvelCoC, cliquez sur le lien ci-dessous.
-    
-    http://localhost:8888/MarvelCoC/assets/php/traitement_verif_mail.php?token=$token
-    
-    Si vous rencontrez des difficultés pour vous connecter à votre compte, contactez-nous à [email contact].
-    
-    Cordialement,
-    L’équipe MarvelCoC";
+    <html>
+        <head>
+            <title>Bonjour $name, merci d’avoir rejoint MarvelCoC.</title>
+        </head>
+        <body style='color:blue'>
+            Nous aimerions vous confirmer que votre compte a été créé avec succès. Pour accéder à MarvelCoC, cliquez sur le lien ci-dessous.
+            
+            http://localhost:8888/MarvelCoC/assets/php/traitement_verif_mail.php?token=$token
+            
+            Si vous rencontrez des difficultés pour vous connecter à votre compte, contactez-nous à [email contact].
+            
+            Cordialement,
+            L’équipe MarvelCoC
+        </body>
+    </html>";
 
-    $headers = "From :" . $from;
-    mail($to,$subject,$message, $headers);
+    $headers[] = "MIME-Version: 1.0";
+    $headers[] = "Content-type: text/html; charset=iso-8859-1";
+    $headers[] = "From: 'MarvelCoC' <anthony.tourolle08@gmail.com>";
+
+    mail($to,$subject,$message, implode("\r\n", $headers));
 ?>
 
