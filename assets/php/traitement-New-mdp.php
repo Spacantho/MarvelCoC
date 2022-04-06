8b5f3a487621e0d6db4737e65196e145a6ed1230
erwantaupin <taupin.erwan@laposte.net>
2022-04-06 09:59:45 +0200

    erreur a regler

--- assets/php/traitement-New-mdp.php
+++ assets/php/traitement-New-mdp.php
@@ -1,1 +1,11 @@
 <?php
+require("db.php");
+
+if(isset($_GET['token'])){
+    if(!empty($_POST['New-mdp']) && !empty($_POST['RNew-mdp'])){
+        if(strlen($_POST['New-mdp']) && strlen($_POST['RNew-mdp']) <= $max){
+
+        }
+    }
+    else{ header('location: ../../pagenewmdp.php?token='echo $_GET["token"];'&erreur=3');}
+}
