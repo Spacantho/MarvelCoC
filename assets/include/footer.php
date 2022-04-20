d851f666c817525284d266bcee168ca85e99e750
erwantaupin <taupin.erwan@laposte.net>
2022-04-20 16:17:10 +0200

    footer-bottom

--- assets/include/footer.php
+++ assets/include/footer.php
@@ -1,7 +1,7 @@
         <footer>
         <div class="body-footer">
             <div class="box-footer">
-                <div class="copy">&copy; <p class='textefooter'>Copyrights by Mr </p><a class='textefooter' href=""> Moulin, </a><p> </p><a class='textefooter' href="">Genonceau, </a><p> </p><a class='textefooter' href="">Tourolle </a><p> et </p><a class='textefooter' href="https://erwan.simplon-charleville.fr/mon-portfolio/mon-folio.php?envoi=1">Taupin </a></div>
+                <div class="copy">&copy; <p class='textefooter'>Copyrights by Mr <a class='textefooter' href=""> Moulin, </a><p> </p><a class='textefooter' href="">Genonceau, </a><p> </p><a class='textefooter' href="">Tourolle </a><p> et </p><a class='textefooter' href="https://erwan.simplon-charleville.fr/mon-portfolio/mon-folio.php?envoi=1">Taupin </a></div>
                 <div class="link">
                     <a href=""><img class="logo-reseaux" src="https://fakeimg.pl/250x100/"></a>
                     <a href=""><img class="logo-reseaux" src="https://fakeimg.pl/250x100/"></a>
