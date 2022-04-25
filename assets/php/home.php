9e697b40a613a8e881712e4d7f0e3f000e7e51c3
erwantaupin <taupin.erwan@laposte.net>
2022-04-25 16:24:22 +0200

    homepage

--- assets/php/home.php
+++ assets/php/home.php
@@ -2,7 +2,7 @@
 session_start();
 if(isset($_SESSION['sess_user_name']) && $_SESSION['sess_user_id'] != "" && $_SESSION['isVerified']) {
   $_SESSION['isconnected'] = true;
-    header('Location:../../categories.php');
+    header('Location:../../homepage.php');
 } else {
     header('Location:../../index.php?log_error=error_verified');
 }
--- homepage.php
+++ homepage.php
@@ -14,25 +14,70 @@ if (!isset($_SESSION) || empty($_SESSION)) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/homepage.css">
+    <link rel="stylesheet" href="assets/css/homepage.css"/>
     <link rel="stylesheet" href="assets/css/navbar.css">
     <link rel="stylesheet" href="assets/css/footer.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="assets/css/slick.css"/>
+    <link rel="stylesheet" type="text/css" href="assets/css/slick-theme.css"/>
     <title>Home</title>
 </head>
-
 <body>
-
-    <?php require_once "assets/include/footer.php"; ?>
-
-        <div id="corps">
-
-
-
-        </div>
-
     <?php require_once "assets/include/navbar.php"; ?>
-
+    <div class="box-nos-films">
+        <div class="sliders onback">
+            <div class="sous-titre"><h3>Nos dernieres videos</h3></div>
+            <div class="slide responsive onrad">
+                <div class="body-slide" id="">
+                    <div class="card" style=" background: url(assets/images/fake.png)">
+                        <a href="" class="js-modal">                           
+                            <div class="textcache" id="info">
+                                <div class="minititre">
+                                    <h4>Info</h4>
+                                    <ul>
+                                        <li><p>test</p></li>
+                                        <li><p>test</p></li>
+                                        <li><p>test</p></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            test
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="sliders onback">
+            <div class="sous-titre"><h3>Videos les plus aimer</h3></div>
+            <div class="slide responsive onrad">
+                <div class="body-slide" id="">
+                    <div class="card" style=" background: url(assets/images/fake.png)">
+                        <a href="" class="js-modal">                           
+                            <div class="textcache" id="info">
+                                <div class="minititre">
+                                    <h4>Info</h4>
+                                    <ul>
+                                        <li><p>test</p></li>
+                                        <li><p>test</p></li>
+                                        <li><p>test</p></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            test
+                        </a>
+                    </div>
+                </div> 
+            </div>
+        </div> 
+    </div>
+    <?php require_once "assets/include/footer.php"; ?>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
     <script src="assets/js/navbar.js"></script>
+    <script src="assets/js/slider.js"></script>
+    <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    <script type="text/javascript" src="assets/js/slick.min.js"></script>
+    <script type="text/javascript" src="assets/js/slick.js"></script>
 </body>
-
 </html>
