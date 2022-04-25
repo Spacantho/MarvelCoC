c042f54d779708487261ef71d5be0cb36cf4aae7
Eldryr62570 <jordan.moulin62570@gmail.com>
2022-04-25 15:25:35 +0200

    Cooldown général sur les vues

--- assets/php/countview.php
+++ assets/php/countview.php
@@ -2,18 +2,29 @@
 //	O:5:"Video":1:{s:11:"*_nbrView";i:0;}
 require("video.php");
 
-
 $sqlRequest = "SELECT view FROM video WHERE id_video = ?";
 $pdoStat = $db->prepare($sqlRequest);
 $pdoStat->execute([$response['id_video']]);
 $videos = $pdoStat->fetch();
 
-$currentView = unserialize($videos[0]);
 
-//Pour appeler la fonction __wakeup() donc rajouter une vue
-$addView = serialize($currentView);
+
+
+
+$currentView = unserialize($videos[0]);
 //Nombre de vues total de la vidéo à afficher
 $views = $currentView->getNbrView();
+$viewDate = strtotime($currentView->getTimestamp());
+$currentDate = time();
+$diff = $viewDate - $currentDate;
+if ($diff <= 0) {
+    $currentView->setDate(0);
+}
+
+var_dump($diff);
+
+$addView = serialize($currentView);
+
 
 //On écrase l'anciennne instance sérialisée
 $sqlRequest = "UPDATE video SET view = ?  WHERE id_video = ?";
