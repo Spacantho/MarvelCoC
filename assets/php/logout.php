2233fb60308bbb31f54e60f8ec4ab063ccd8436a
Spacantho <anthony.tourolle08@gmail.com>
2022-04-26 11:55:02 +0200

    contact fix / logout cookie fix

--- assets/php/logout.php
+++ assets/php/logout.php
@@ -2,4 +2,13 @@
 session_start();
 session_destroy();
 
+if (isset($_COOKIE['remember'])) {
+    unset($_COOKIE['remember']['id_users']);
+    unset($_COOKIE['remember']['token']);
+
+    setcookie("remember[token]", null,  -1, "/");
+    setcookie("remember[id_users]", null,  -1, "/");
+}
+
 header('location: ../../index.php');
+?>
