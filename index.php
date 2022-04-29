16ff9f2c05f8507e87e87c7bd1f9b853274415c2
Spacantho <98018883+Spacantho@users.noreply.github.com>
2022-04-29 11:23:57 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>INSERER NOM</title>
+    <title>MarvelCoC</title>
     <link rel="stylesheet" href="assets/css/index.css">
     <link rel="stylesheet" href="assets/css/navbar.css">
     <link rel="stylesheet" href="assets/css/footerIndex.css">
